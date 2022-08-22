eb7d167170f028e30a112db4ec5fd3b8db8d5ffd
Eshan Rajapakshe <eshan.r@aventude.com>
2022-08-22 09:28:30 +0530

    Update home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,47 @@
+// SHOW/HIDE NAV
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+
+$(function () {
+  if (lastScrollTop < 10) {
+    $(".nav-header").addClass("nav-top");
+  }
+});
+
+$(window).scroll(function (event) {
+  didScroll = true;
+});
+
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+  if (Math.abs(lastScrollTop - st) <= delta) return;
+  var navbarHeight = $(".nav-header").outerHeight();
+  if (st > lastScrollTop && st > navbarHeight) {
+    $(".nav-header").removeClass("show-nav").addClass("hide-nav");
+  } else {
+    if (st + $(window).height() < $(document).height()) {
+      $(".nav-header").removeClass("hide-nav").addClass("show-nav");
+      if ($(window).scrollTop()) {
+        $(".nav-header").addClass("nav-shadow");
+        $(".nav-header").removeClass("nav-top");
+      } else {
+        $(".nav-header").removeClass("nav-shadow");
+        $(".nav-header").addClass("nav-top");
+      }
+    }
+  }
+
+  lastScrollTop = st;
+}
+
 $(function () {
   $(".selectpicker").selectpicker();
 
@@ -32,4 +76,57 @@ $(function () {
     const selected = $(e.currentTarget).val();
     allagiglossas(selected);
   });
+
+  $(".owl-carousel").on("initialized.owl.carousel", () => {
+    setTimeout(() => {
+      $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
+      $("section").show();
+    }, 200);
+  });
+
+  const $owlCarousel = $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    navText: [
+      '<svg width="20" height="20" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+      '<svg width="20" height="20" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */,
+    ],
+  });
+
+  $owlCarousel.on("changed.owl.carousel", (e) => {
+    $(".owl-slide-animated").removeClass("is-transitioned");
+
+    const $currentOwlItem = $(".owl-item").eq(e.item.index);
+    $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
+
+    const $target = $currentOwlItem.find(".owl-slide-text");
+    doDotsCalculations($target);
+  });
+
+  $owlCarousel.on("resize.owl.carousel", () => {
+    setTimeout(() => {
+      setOwlDotsPosition();
+    }, 50);
+  });
+
+  setOwlDotsPosition();
+
+  function setOwlDotsPosition() {
+    const $target = $(".owl-item.active .owl-slide-text");
+    doDotsCalculations($target);
+  }
+
+  function doDotsCalculations(el) {
+    const height = el.height();
+    const { top, left } = el.position();
+    const res = height + top + 20;
+
+    $(".owl-carousel .owl-dots").css({
+      top: `${res}px`,
+      left: `${left}px`,
+    });
+  }
 });
